c0aef8053d770e803f2a0586ff30572b1fb9c85c
n-paternoster <paternosterniklas@yahoo.de>
2021-11-02 14:57:48 +0100

    Add http auth

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ const path = require("path");
 const helmet = require("helmet");
 const { body, validationResult } = require('express-validator');
 require('dotenv').config()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
+//Passwortsachen
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+const users = [{
+    id: '1635860869289',
+    name: 'Pada',
+    email: 'paternosterniklas@yahoo.de',
+    password: process.env.hashed
+}
+]
 
 
 //Alles DatenBank
@@ -29,13 +48,23 @@ app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"))
 app.set("public", path.join(__dirname, "/public"))
 
 
-app.get('/', (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
 
     res.render("FitnessTracker")
 
@@ -128,7 +157,62 @@ app.get('/Chart', (req, res) => {
 })
 
 
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
 
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+
+}))
+
+// app.get('/register', checkNotAuthenticated, (req, res) => {
+//     res.render('register.ejs')
+// })
+
+// app.post('/register', checkNotAuthenticated, async (req, res) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//         users.push({
+//             id: Date.now().toString(),
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword
+
+//         })
+//         console.log(users)
+//         res.redirect('/login')
+//     } catch {
+//         res.redirect('/register')
+//     }
+// })
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 app.listen(80, () => {
     console.log("Listening on Port 80")

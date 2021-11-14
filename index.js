9762de9804cc4d8186606516488324e608ae0332
n-paternoster <paternosterniklas@yahoo.de>
2021-11-14 20:38:05 +0100

    EE

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/live/padadev.com/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/padadev.com/fullchain.pem'),
@@ -338,9 +338,9 @@ const httpsServer = https.createServer({
 
 
 
-httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80');
-});
+// httpServer.listen(80, () => {
+//     console.log('HTTP Server running on port 80');
+// });
 
 httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443');

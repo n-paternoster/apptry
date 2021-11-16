c86a94fa59db332904fd9136cc06db93e7a42f8a
n-paternoster <paternosterniklas@yahoo.de>
2021-11-17 00:09:19 +0100

    Fix BUg 2

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.get("/Datenbank", checkAuthenticated, async (req, res) => {
 
                 nameArray.push(name)
                 const daybefore = await Daten.find({ username: Uname, exerciseName: name, exerciseDate: { $ne: Day }, basicExercise: false }, 'exerciseDate').sort({ $natural: -1 }).limit(1);
-                if (typeof daybefore !== 'undefined') {
+                if (typeof daybefore[0].exerciseDate !== 'undefined') {
                     // the variable is defined
                     let beforeday = daybefore[0].exerciseDate;
                     const pDaten = await Daten.find({ username: Uname, exerciseName: name, basicExercise: false, exerciseDate: beforeday }, 'exerciseWeight exerciseRep exerciseSet')

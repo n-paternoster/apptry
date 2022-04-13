6a92b5fb92b6b977e4a06c03af6ac7f86cc3056a
n-paternoster <paternosterniklas@yahoo.de>
2022-04-14 00:24:42 +0200

    Fix Promise

--- public/js/handler.js
+++ public/js/handler.js
@@ -1,36 +1,37 @@
 const weightinputElement = document.getElementById("saveWeightButton")
-weightinputElement.addEventListener("click", (evt) => {
-    if (weightinputElement.previousElementSibling.value.length != 0) {
+if (weightinputElement) {
+    weightinputElement.addEventListener("click", (evt) => {
+        if (weightinputElement.previousElementSibling.value.length != 0) {
 
-        let weightData = evt.target.previousElementSibling.value
-        let today = new Date();
-        let eDate = today.toISOString().slice(0, 10);  //aktuelles Datum in Year/month/day
-        let dataObject = {
-            weightDate: eDate,
-            weight: weightData
-        }
-
-
-        axios({
-            method: 'post',
-            url: '/Datenbank/newWeight',
-            data: dataObject,
-            headers: {
-                'Content-Type': 'application/json'
+            let weightData = evt.target.previousElementSibling.value
+            let today = new Date();
+            let eDate = today.toISOString().slice(0, 10);  //aktuelles Datum in Year/month/day
+            let dataObject = {
+                weightDate: eDate,
+                weight: weightData
             }
-        })
-            .then((response) => {
 
-            }, (error) => {
-                console.log(error);
-            });
 
-        let accordion = document.getElementById("weightAcc")
-        accordion.remove()
+            axios({
+                method: 'post',
+                url: '/Datenbank/newWeight',
+                data: dataObject,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((response) => {
 
-    }
-})
+                }, (error) => {
+                    console.log(error);
+                });
 
+            let accordion = document.getElementById("weightAcc")
+            accordion.remove()
+
+        }
+    })
+}
 const overviewButton = document.getElementById("overviewButton")
 
 overviewButton.addEventListener("click", async (evt) => {
